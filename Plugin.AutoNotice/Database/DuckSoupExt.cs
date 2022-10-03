5dd6e908f21e3695fcde3660e69a7f4a8e815ed1
b0ykoe <b0ykoe@ymail.com>
2022-10-03 18:55:38 +0200

    remove plugins

--- Plugin.AutoNotice/Database/DuckSoupExt.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Data.Entity;
-using API.Database.DuckSoup;
-
-namespace Plugin.AutoNotice.Database;
-
-public class DuckSoupExt : DuckSoup
-{
-    public virtual DbSet<AutoNoticeTable> AutoNotice { get; set; }
-}
--- Plugin.DatabaseCommands/Database/DuckSoupExt.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Data.Entity;
-using API.Database.DuckSoup;
-
-namespace Plugin.DatabaseCommands.Database;
-
-public class DuckSoupExt : DuckSoup
-{
-    public virtual DbSet<DatabaseCommandTable> DatabaseCommand { get; set; }
-}
