24a2ce99e961c44a57c32535a54297ab316a3a62
b0ykoe <b0ykoe@ymail.com>
2023-02-04 12:31:38 +0100

    fix: crash while using mercenaries

--- DuckSoup/Library/Objects/Spawn/SpawnedCos.cs
+++ DuckSoup/Library/Objects/Spawn/SpawnedCos.cs
@@ -3,14 +3,18 @@
 namespace DuckSoup.Library.Objects.Spawn;
 
 // https://github.com/SDClowen/RSBot/
+// https://www.elitepvpers.com/forum/sro-coding-corner/3970615-release-characterdata-entityspawn.html
 public sealed class SpawnedCos : SpawnedNpc
 {
     public string Name { get; set; }
     public string GuildName { get; set; }
     public string OwnerName { get; set; }
     public uint OwnerUniqueId { get; set; }
+
     public SpawnedCos(uint objId) :
-        base(objId) { }
+        base(objId)
+    {
+    }
 
     internal override void Deserialize(Packet packet)
     {
@@ -28,44 +32,64 @@ public sealed class SpawnedCos : SpawnedNpc
             || refObj.TypeID4 == 8 //NPC_COS_QUEST
             || refObj.TypeID4 == 9) // COS_PET2
         {
-            if (refObj.TypeID4 == 3 ||  // NPC_COS_P_GROWTH
-                refObj.TypeID4 == 4 ||  //NPC_COS_P_ABILITY
-                refObj.TypeID4 == 9)    // COS_PET2
-                Name = packet.ReadAscii();
-            else if (refObj.TypeID4 == 5) //NPC_COS_GUILD
+            if (refObj.TypeID4 == 5)
+            {
+                //NPC_COS_GUILD
                 GuildName = packet.ReadAscii();
-
-            if (refObj.TypeID4 == 2     //NPC_COS_TRASNPORT
-                || refObj.TypeID4 == 3  //NPC_COS_P_GROWTH
-                || refObj.TypeID4 == 4  //NPC_COS_P_ABILITY
-                || refObj.TypeID4 == 5  //NPC_COS_GUILD
-                || refObj.TypeID4 == 6  //NPC_COS_CAPTURED
-                || refObj.TypeID4 == 9) //COS_PET2
+            }
+            else
             {
                 OwnerName = packet.ReadAscii();
+            }
 
-                if (refObj.TypeID4 == 2 //NPC_COS_TRASNPORT
-                    || refObj.TypeID4 == 3 //NPC_COS_P_GROWTH
-                    || refObj.TypeID4 == 4 //NPC_COS_ABILITY
-                    || refObj.TypeID4 == 5 //NPC_COS_GUILD
-                    || refObj.TypeID4 == 9) //COS_PET2 
+            // if (refObj.TypeID4 == 3 ||  // NPC_COS_P_GROWTH
+            //     refObj.TypeID4 == 4 ||  //NPC_COS_P_ABILITY
+            //     refObj.TypeID4 == 9)    // COS_PET2
+            //     Name = packet.ReadAscii();
+            // else if (refObj.TypeID4 == 5) //NPC_COS_GUILD
+            //     GuildName = packet.ReadAscii();
+            //
+            // if (refObj.TypeID4 == 2     //NPC_COS_TRASNPORT
+            //     || refObj.TypeID4 == 3  //NPC_COS_P_GROWTH
+            //     || refObj.TypeID4 == 4  //NPC_COS_P_ABILITY
+            //     || refObj.TypeID4 == 5  //NPC_COS_GUILD
+            //     || refObj.TypeID4 == 6  //NPC_COS_CAPTURED
+            //     || refObj.TypeID4 == 9) //COS_PET2
+            // {
+            //     OwnerName = packet.ReadAscii();
+
+            if (refObj.TypeID4 == 2 //NPC_COS_TRASNPORT
+                || refObj.TypeID4 == 3 //NPC_COS_P_GROWTH
+                || refObj.TypeID4 == 4 //NPC_COS_ABILITY
+                || refObj.TypeID4 == 5 //NPC_COS_GUILD
+                || refObj.TypeID4 == 9) //COS_PET2 
+            {
+                packet.ReadUInt8(); //Owner job type
+
+                // if (refObj.TypeID4 == 2 //NPC_COS_TRASNPORT
+                //     || refObj.TypeID4 == 3 //NPC_COS_P_GROWTH
+                //     || refObj.TypeID4 == 5 //NPC_COS_GUILD
+                //     || refObj.TypeID4 == 9) //COS_PET2 
+                // {
+                //     packet.ReadUInt8(); //Owner PVP state
+                //
+                //     if (refObj.TypeID4 == 5) //NPC_COS_GUILD
+                //     {
+                //         packet.ReadUInt32(); //OwnerRefID guild foo
+                //     }
+                // }
+                if (refObj.TypeID4 != 4) //NO NPC_COS_P (Ability)
                 {
-                    packet.ReadUInt8(); //Owner job type
-
-                    if (refObj.TypeID4 == 2 //NPC_COS_TRASNPORT
-                        || refObj.TypeID4 == 3 //NPC_COS_P_GROWTH
-                        || refObj.TypeID4 == 5 //NPC_COS_GUILD
-                        || refObj.TypeID4 == 9) //COS_PET2 
-                    {
-                        packet.ReadUInt8(); //Owner PVP state
-
-                        if (refObj.TypeID4 == 5) //NPC_COS_GUILD
-                        {
-                            packet.ReadUInt32(); //OwnerRefID guild foo
-                        }
-                    }
+                    packet.ReadUInt8(); //  MurderFlag    //0 = White, 1 = Purple, 2 = Red
+                }
+                
+                if(refObj.TypeID4 == 5)
+                {
+                    //NPC_COS_GUILD
+                    packet.ReadUInt32(); // Owner.RefObjID
                 }
             }
+            // }
 
             OwnerUniqueId = packet.ReadUInt32();
             if (refObj.TypeID4 == 9)
