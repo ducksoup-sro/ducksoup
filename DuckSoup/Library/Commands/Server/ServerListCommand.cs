2a371f7f3f5b1ba35ba1277aa5a2da6dae69f559
b0ykoe <b0ykoe@ymail.com>
2025-03-29 00:44:01 +0100

    feat: reenable small server list command

--- DuckSoup/Library/Commands/Server/ServerListCommand.cs
+++ DuckSoup/Library/Commands/Server/ServerListCommand.cs
@@ -1,6 +1,9 @@
-﻿using API.Command;
+﻿using System.Linq;
+using API.Command;
 using API.Server;
 using API.ServiceFactory;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 namespace DuckSoup.Library.Commands.Server;
 
@@ -15,12 +18,20 @@ public class ServerListCommand : Command
 
     public override void Execute(string[] args)
     {
-        // foreach (var asyncServer in _serverManager.Servers)
-        // {
-        //     Log.Information("Id: {0} - Name: {1} - Started {2} - Type: {3} ",asyncServer.Service.ServiceId,
-        //         asyncServer.Service.Name,
-        //         asyncServer.Started,
-        //         asyncServer.Service.ServerType);
-        // }
+        
+        using var context = new API.Database.Context.DuckSoup();
+        var services = context.Services.Include(b => b.LocalMachine_Machine)
+            .Include(b => b.RemoteMachine_Machine).Include(b => b.SpoofMachine_Machine).ToList();
+
+        foreach (var service in services)
+        {
+            var started = _serverManager.Servers.Any(server => server.Service.ServiceId == service.ServiceId);
+            
+            Log.Information("Id: {0} - Name: {1} - Started {2} - Type: {3} ",
+                service.ServiceId,
+                service.Name,
+                started,
+                service.ServerType);
+        }
     }
 }
